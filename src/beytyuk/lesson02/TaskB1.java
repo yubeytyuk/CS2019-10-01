e0835491d65f64717283edba6f295997b11470f4
Yulya <yubeytyuk@gmail.com>
2019-10-07 01:47:38 +0300

    Lesson 2

---
+++ src/beytyuk/lesson02/TaskB1.java
@@ -0,0 +1,36 @@
+package beytyuk.lesson02;
+
+/*
+Выводим квадрат числа
+Напишите программу, которая считывает с клавиатуры целое число a и выводит квадрат этого числа (a * a).
+Для считывания данных с клавиатуры используйте метод nextInt() объекта класса Scanner.
+
+Создать Scanner можно так:
+Scanner sc=new Scanner(System.in);
+
+Прочитать число можно так:
+int i=sc.nextInt();
+
+Требования:
+1. Программа должна выводить текст.
+2. В программе необходимо создать объект типа Scanner.
+3. Программа должна считывать число типа Int с клавиатуры.
+4. Программа должна выводить квадрат считанного числа.
+
+ */
+
+import java.util.Scanner;
+
+class TaskB1 {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("a");
+        if (sc.hasNext()) {
+            a= sc.nextInt();
+            System.out.println(a*a);
+        }
+
+
+    }
+}
---
+++ src/beytyuk/lesson02/TaskC1.java
@@ -0,0 +1,31 @@
+package beytyuk.lesson02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и выводит их сумму на экран в виде
+
+Ввод (это вы вводите с клавиатуры):
+34 26
+
+Вывод (это должна появится в консоли, обратите внимание на пробелы и слово Sum:
+Sum = 60
+
+*/
+class TaskC1 {
+    public static void main(String[] args) {
+        int[] array= {34,26};
+        int sum=0;
+        for (int num: array) {
+            sum=sum+num;
+            System.out.println("60+sum");
+    }
+
+    }
+
+
+
+
+
+}
